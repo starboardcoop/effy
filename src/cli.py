af05110fb0180da5f03cf5c6df7665cd520570d3
Dillon Fagan <dillonfagan@pm.me>
2020-07-19 22:43:23 -0400

    Add share calculation

--- src/cli.py
+++ src/cli.py
@@ -4,13 +4,16 @@ from orgdata import OrgData
 class Cli:
     def __init__(self):
         self.data = OrgData.load()
+        self.total_points = 0
+        self.personal_points = []
 
     def run(self):
         self.list_names()
         print("Please enter a person's name and their effort ratings, separated by spaces.")
         print("Add a comma to the end of each line to continue to the next person.")
         ratings = self.get_ratings()
-        print(ratings)
+        self.calculate_points(ratings)
+        self.calculate_shares()
 
     def list_names(self):
         text = 'People: '
@@ -28,3 +31,16 @@ class Cli:
             if not input_line.endswith(','):
                 break
         return ratings
+    
+    def calculate_points(self, ratings):
+        for rating in ratings:
+            frags = rating.split(' ')
+            name = frags[0]
+            points = sum(list(map(int, frags[1:])))
+            self.personal_points.append((name, points))
+            self.total_points += points
+    
+    def calculate_shares(self):
+        for person in self.personal_points:
+            share = (person[1] / self.total_points) * 100
+            print(f"{person[0]} -> {share}%")
