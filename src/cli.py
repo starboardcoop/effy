559ccb609bc45604ecffb7bd8cef311fb576abc3
Dillon Fagan <dillonfagan@pm.me>
2020-07-19 22:20:47 -0400

    Accept ratings input

--- src/cli.py
+++ src/cli.py
@@ -9,6 +9,8 @@ class Cli:
         self.list_names()
         print("Please enter a person's name and their effort ratings, separated by spaces.")
         print("Add a comma to the end of each line to continue to the next person.")
+        ratings = self.get_ratings()
+        print(ratings)
 
     def list_names(self):
         text = 'People: '
@@ -17,3 +19,12 @@ class Cli:
             if i < len(self.data.people) - 1:
                 text += ' | '
         print(text)
+    
+    def get_ratings(self):
+        ratings = []
+        while True:
+            input_line = input('<- ')
+            ratings.append(input_line.replace(',', ''))
+            if not input_line.endswith(','):
+                break
+        return ratings
