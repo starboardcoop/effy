42ff0c0bb981524917e5313fa0b1936dc24e5bc1
Dillon Fagan <dillonfagan@pm.me>
2020-07-19 22:09:09 -0400

    Add prompt with directions

--- src/cli.py
+++ src/cli.py
@@ -7,6 +7,8 @@ class Cli:
 
     def run(self):
         self.list_names()
+        print("Please enter a person's name and their effort ratings, separated by spaces.")
+        print("Add a comma to the end of each line to continue to the next person.")
 
     def list_names(self):
         text = 'People: '
