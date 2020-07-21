365c700024ada8bfdd310913d909f1e831f01901
Dillon Fagan <dillonfagan@pm.me>
2020-07-21 12:28:56 -0400

    Add descriptions to help menu

--- src/effy.py
+++ src/effy.py
@@ -4,10 +4,12 @@ from cli import Cli
 
 @click.group()
 def main():
+    """A simple app for calculating someone's contribution to a project."""
     pass
 
 @main.command()
 def calc():
+    """Starts the app."""
     Cli().run()
 
 if __name__ == '__main__':
